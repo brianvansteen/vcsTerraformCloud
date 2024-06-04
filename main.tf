e68abc789b57116234f8eeaf027306532e72b014
Brian Van Steen <bvansteen@gmail.com>
2024-06-04 08:32:10 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,150 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+  alias   = "east"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+  alias   = "west"
+}
+
+locals {
+  ingress_rules = [{
+    port        = 443
+    description = "HTTPS 443"
+    },
+    {
+      port        = 80
+      description = "HTTP 80"
+  }]
+}
+
+data "aws_vpc" "main" { # data sources defined outside of Terraform
+  id = "vpc-da5530bc"
+}
+
+
+data "aws_ami" "east-amazon-linux-2" {
+  provider    = aws.east
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+data "aws_ami" "west-amazon-linux-2" {
+  provider    = aws.west
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "my_east_server" {
+  ami           = data.aws_ami.east-amazon-linux-2.id
+  instance_type = var.instance_type
+  provider      = aws.east
+  tags = {
+    Name = "Server-East"
+  }
+}
+
+resource "aws_instance" "my_west_server" {
+  ami           = data.aws_ami.west-amazon-linux-2.id
+  instance_type = var.instance_type
+  provider      = aws.west
+  tags = {
+    Name = "Server-East"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_instance" "importedServer" {
+  ami           = data.aws_ami.west-amazon-linux-2.id
+  instance_type = var.instance_type
+  provider      = aws.west
+}
+
+resource "aws_security_group" "inbound" {
+  name = "security_group"
+  # vpc_id = data.aws_vpc.main.id
+  vpc_id = var.vpc_id
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+    content {
+      description      = ingress.value.description
+      from_port        = ingress.value.port
+      to_port          = ingress.value.port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = ["sg-a77e10db"]
+      self             = false
+    }
+  }
+
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer_key"
+  public_key = var.public_key
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+output "east_public_ip" {
+  value = aws_instance.my_east_server.public_ip
+}
+output "west_public_ip" {
+  value = aws_instance.my_west_server.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_key" {
+    type = string
+}
